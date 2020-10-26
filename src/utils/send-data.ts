e9ae5b997c8bab904f36ae79bb63ad09f7c0219d
sebastien-lb <sebast.lubineau@laposte.net>
2020-10-26 22:50:33 +0100

    test link

--- src/utils/send-data.ts
+++ src/utils/send-data.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const sendData = (kcal: number): (() => void) => () => {
-  const BASE_URL = process.env.REACT_APP_BACK_URL as string;
-
   axios.post(`${window.location.host}/.netlify/function/fauna`, { kcal: kcal });
 };
