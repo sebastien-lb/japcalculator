dcdedb360f2802504a855bfa7b0df11206b24c7d
Alexandre Alves <alexandre.alves@supelec.fr>
2020-11-10 09:33:37 +0100

    nouvelle branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CalculatorPage } from "./pages";
 const App = () => {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Nouvelle branche</h1>
       <CalculatorPage />
     </div>
   );
