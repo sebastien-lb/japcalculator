74d0530097bc78940119235002d14a94129954e9
rom1bl <romain.blanc@hey.com>
2020-11-10 09:38:11 +0100

    useless commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import { CalculatorPage } from "./pages";
 
+//Useless comment
 const App = () => {
-  return (
-    <div className="App">
-      <CalculatorPage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <CalculatorPage />
+        </div>
+    );
 };
 
 export default App;
