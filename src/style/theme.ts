a5c1147fc6bce1a719990ccb560f96dadb9537d0
Clement Dessoude <clement.dessoude@gmail.com>
2021-10-12 15:27:34 +0200

    feat: change button color

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -5,22 +5,21 @@ export interface CustomTheme extends Theme {}
 export const theme: CustomTheme = createMuiTheme({
   palette: {
     primary: {
-      main: "#F9C41C"
-    }
+      main: "#F9C41C",
+    },
   },
   overrides: {
     MuiButton: {
       root: {
-        background:
-          "transparent linear-gradient(101deg, #F78E0D 0%, #F9C41C 100%) 0% 0% no-repeat padding-box"
+        background: "transparent linear-gradient(101deg, #A78EAD 0%, #F9C41C 100%) 0% 0% no-repeat padding-box",
       },
       contained: {
-        color: "white",
-        boxShadow: "0px 2px 20px #00000029"
+        color: "blue",
+        boxShadow: "0px 2px 20px #00000029",
       },
       label: {
-        textTransform: "none"
-      }
-    }
-  }
+        textTransform: "none",
+      },
+    },
+  },
 });
