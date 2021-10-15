718076605a05cdb2e25d38c11d73a2272d78ae02
Alex Quilloux <74928088+Alekso91@users.noreply.github.com>
2021-10-15 13:56:33 +0200

    Update Calculator.tsx

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -79,7 +79,7 @@ export const CalculatorPage: React.FC<Props> = (props: Props) => {
           </div>
           <div className={classes.buttonContainer}>
             <Button variant="contained" onClick={sendData(result)}>
-              {result} Kcal
+              {result} too much cal
             </Button>
           </div>
         </div>
