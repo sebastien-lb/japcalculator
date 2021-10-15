345127c32546c2ea407548910ac79f983647f063
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2021-10-15 13:06:41 +0200

    Error handling (try... catch)

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,26 +1,29 @@
 exports.handler = async function(event, context) {
   console.log(event);
 
-  if (event.httpMethod === "OPTIONS") {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    };
-    return {
-      statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
-      headers,
-      body: "This was a preflight call!",
-    };
-  }
-
   try { 
     const requestBody = JSON.parse(event.body);
     const message = requestBody.message;
+
+    if (event.httpMethod === "OPTIONS") {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      };
+      return {
+        statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
+        headers,
+        body: "This was a preflight call!",
+      };
+    }
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: message }),
     };
+
+  
   }
   catch (error) {
     return {
