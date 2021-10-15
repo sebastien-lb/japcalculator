e16dc26b142c00083267c3c55b0e7e8bd2527b6d
clarani <clara.ni@student-cs.fr>
2021-10-15 15:05:13 +0200

    Corrections 2

--- src/components/CalculatorModal/CalculatorModal.tsx
+++ src/components/CalculatorModal/CalculatorModal.tsx
@@ -68,7 +68,7 @@ export const CalculatorModal: React.FC<Props> = (props: Props) => {
           ))}
         </div>
         <div className={classes.buttonContainer}>
-          <Button variant="contained">{result} Kcal !</Button>
+          <Button variant="contained">{result} Kcal</Button>
         </div>
       </DialogContent>
     </Dialog>
