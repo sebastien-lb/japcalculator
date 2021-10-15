61ec8092f77ce1a6a96cfcf6f67c9214cc9e24fb
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2021-10-15 15:42:57 +0200

    New feature in teapot

--- functions/teapot.js
+++ functions/teapot.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
   return {
     statusCode: 418,
-    body: JSON.stringify({ message: "Hello" }),
+    body: JSON.stringify({ message: "Hello, I'm a teapot for the new feature" }),
   };
 };
