d36936cc524315233172f6a689900f9eb9368e77
jean <jean.bourgeois71@gmail.com>
2021-10-15 15:02:34 +0200

    fix path

--- src/data/food.ts
+++ src/data/food.ts
@@ -4,7 +4,7 @@ import Maki from "../assets/images/food/Maki.svg";
 import Sushi from "../assets/images/food/Sushi.svg";
 import California from "../assets/images/food/California.svg";
 import BeefCheese from "../assets/images/food/Brochette boeuf fro.svg";
-import Sumo from "../assets/images/food/sumo.png";
+import Sumo from "../assets/images/sumo.png";
 
 
 export interface FoodItem {
