b1aa9f19d70f3af5319433fc97f3a12bbcd790a7
EwenQuim <ewen.quimerch@gmail.com>
2021-10-22 14:33:26 +0200

    on a mis le rond rouge en bleu

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -119,7 +119,7 @@ const styles = (theme: CustomTheme): Record<ClassNames, CSSProperties> => ({
     height: "calc(100vh - 40px)",
     width: "calc(100vh - 40px)",
     borderRadius: "50%",
-    backgroundColor: "#BF2424",
+    backgroundColor: "#4336F4",
   },
   kanjiSun: {
     position: "absolute",
